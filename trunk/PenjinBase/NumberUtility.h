a565adeeacd4d8e47a698fe121fd5d2dca112d0d
foxblock@gmail.com <foxblock@gmail.com@10f69238-722c-11de-8fed-f74124958806>
2009-09-18 21:05:32 +0000

    Modified NumberUtility: [Add] Signum function [Add] Simple prime checking function (not optimized) [Add] Some statistic functions for arrays of integers [Change] Layout, clean-up [Remove] Removed redundant which are part of the standard library: min, max, roundUp, roundDown (use min, max, ceil, floor instead) [Remove] Removed redundant include

--- trunk/PenjinBase/NumberUtility.h
+++ trunk/PenjinBase/NumberUtility.h
@@ -1,32 +1,37 @@
 #ifndef NUMBERUTILITY_H_INCLUDED
 #define NUMBERUTILITY_H_INCLUDED
-#include <cmath>
+
 #include "PenjinTypes.h"
 
 namespace NumberUtility
 {
+    /// analysis
+
 #ifdef PENJIN_FIXED
     Fixed linearInterpolate(CRint x, CRint y, CRFixed step);
-    int roundUp(CRFixed x);
-    int roundDown(CRFixed x);
 #else
     float linearInterpolate(CRint x, CRint y, CRfloat step);
-    int roundUp(CRfloat x);
-    int roundDown(CRfloat x);
 #endif
-    template <class T>
-    T min(const T& a, const T& b){return a<b ? a : b;}
-    template <class T>
-    T max(const T& a, const T& b){return a>b ? a : b;}
+
     template <class T>
     T limit(const T& x, const T& minV, const T& maxV){return min( maxV, max(minV,x));}
     bool isMultiple(CRint x,CRint y);
-    int nextPowerOfTwo(CRint x);
-    // Check if a value is a power of two
     bool isPowerOfTwo(CRint x);
+    bool isPrime(CRint value);
+    int nextPowerOfTwo(CRint x);
+    template <class T>
+    int sign(const T& value);   // Signum function, returns the sign of a number
     int wrapValue(int value, CRint limit);
+    //template<typename T>
+    //T wrapValue(T val,const T& limit);
 
-//        template<typename T>
-//        T wrapValue(T val,const T& limit);
-}
+    /// statistics
+
+    int getGreatest(const int arr[], CRint length);                    // Returns the greatest number in an array
+    int getGreatestIndex(const int arr[], CRint length);               // Returns the index of the greatest number in an array
+    bool isGreatest(const int arr[], CRint index, CRint length);       // Checks if the supplied element is the greatest in an array of numbers
+    //template <class T, int length>
+    //T test(const T (&arr)[length], CRint index);
+
+};
 #endif // NUMBERUTILITY_H_INCLUDED
