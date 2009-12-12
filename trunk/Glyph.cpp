0e53b9c2fd8f17771a8131cf59efccefca2cb363
pokeparadox <pokeparadox@10f69238-722c-11de-8fed-f74124958806>
2009-12-12 14:37:10 +0000

    Updated: Glyph now uses Image type to store the character - easier cross platform maintenance. Updated: Some position added to glyph renderer. (getting garbage characters :()

--- trunk/Glyph.cpp
+++ trunk/Glyph.cpp
@@ -4,30 +4,20 @@ Glyph::Glyph()
 {
     //ctor
     fontSize = 0;       // no font.
-    #ifdef PENJIN_SDL
-        glyph = NULL;
-        screen = SDL_GetVideoSurface();
-    #endif
 }
 
 Glyph::~Glyph()
 {
     //dtor
-    #ifdef PENJIN_SDL
-        if(glyph)
-            SDL_FreeSurface(glyph);
-        glyph = NULL;
-    #endif
 }
 
 
 #ifdef PENJIN_SDL
 void Glyph::render(SDL_Surface* scr)
 {
-    if(glyph)
+    if(glyph.size())
     {
-        SDL_Rect textPos = { position->x, position->y, 0, 0 };
-        SDL_BlitSurface(glyph, NULL, scr,&textPos);
+        glyph.renderImage(scr,*position);
     }
     else if((character != NULL) && fontSize > 0)  //  if there is a character to render, render it.
         refresh();
@@ -39,18 +29,18 @@ void Glyph::refresh()
     #ifdef PENJIN_SDL
         if(position == NULL)        //  No cursor position is invalid usage.
             return;
-        if(glyph)
-            SDL_FreeSurface(glyph);
-        glyph = NULL;
-        glyph = TTF_RenderText_Blended(font, &character, colour.getSDL_Colour());
+        if(glyph.size())
+            glyph.clear();
+        glyph.loadImage( TTF_RenderText_Blended(font, &character, colour.getSDL_Colour()) );
     #endif
 }
 
 void Glyph::render()
 {
-    #ifdef PENJIN_SDL
-        render(screen);
-    #elif PENJIN_GL
-
-    #endif
+    if(glyph.size())
+    {
+        glyph.renderImage(*position);
+    }
+    else if((character != NULL) && fontSize > 0)  //  if there is a character to render, render it.
+        refresh();
 }
