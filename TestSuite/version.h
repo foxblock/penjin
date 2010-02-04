d5ea8d537cd2cdcf3c81ebd4d24a9dd944f1c2dc
pokeparadox <pokeparadox@10f69238-722c-11de-8fed-f74124958806>
2010-02-04 22:42:50 +0000

    Updated: Minor fixes to project file.

--- TestSuite/version.h
+++ TestSuite/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "15";
+	static const char DATE[] = "16";
 	static const char MONTH[] = "01";
 	static const char YEAR[] = "2010";
 	static const double UBUNTU_VERSION_STYLE = 10.01;
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 2;
-	static const long BUILD = 812;
-	static const long REVISION = 4556;
+	static const long BUILD = 819;
+	static const long REVISION = 4593;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 1401;
-	#define RC_FILEVERSION 0,2,812,4556
-	#define RC_FILEVERSION_STRING "0, 2, 812, 4556\0"
-	static const char FULLVERSION_STRING[] = "0.2.812.4556";
+	static const long BUILDS_COUNT = 1412;
+	#define RC_FILEVERSION 0,2,819,4593
+	#define RC_FILEVERSION_STRING "0, 2, 819, 4593\0"
+	static const char FULLVERSION_STRING[] = "0.2.819.4593";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 12;
+	static const long BUILD_HISTORY = 19;
 	
 
 }
