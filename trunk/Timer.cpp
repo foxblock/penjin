a4876845f984527940f610945ad43f950061352d
foxblock <foxblock@10f69238-722c-11de-8fed-f74124958806>
2010-05-07 19:09:47 +0000

    Fixed Timer::getScaledTicks(TimerScaler) permanently settings scaler instead of just returning a differently scaled value Added setOffset and getOffset functions to Timer

--- trunk/Timer.cpp
+++ trunk/Timer.cpp
@@ -1,54 +1,56 @@
 #include "Timer.h"
 
-void Timer::calcScaler()
+#ifdef PENJIN_FIXED
+Fixed Timer::calcScaler(const TimerScalers& mode)
+#else
+float Timer::calcScaler(const TimerScalers& mode)
+#endif
 {
     #ifdef PENJIN_FIXED
         Fixed divider;
-        scaler = 0;
         if (mode == FIFTEEN_FRAMES)
         {
             divider = 15;
-            scaler = Fixed(MY_CLOCKS) / divider;
+            return (Fixed(MY_CLOCKS) / divider);
         }
         else if (mode == THIRTY_FRAMES)
         {
             divider = 30;
-            scaler = Fixed(MY_CLOCKS) / divider;
+            return (Fixed(MY_CLOCKS) / divider);
         }
         else if (mode == FIFTY_FRAMES)
         {
             divider = 50;
-            scaler = Fixed(MY_CLOCKS) / divider;
+            return (Fixed(MY_CLOCKS) / divider);
         }
         else if (mode == SIXTY_FRAMES)
         {
             divider = 60;
-            scaler = Fixed(MY_CLOCKS) / divider;
+            return (Fixed(MY_CLOCKS) / divider);
         }
     #else
-        scaler = 0;
         if (mode == FIFTEEN_FRAMES)
-            scaler = (float)MY_CLOCKS / 15.0f;
+            return ((float)MY_CLOCKS / 15.0f);
         else if (mode == THIRTY_FRAMES)
-            scaler = (float)MY_CLOCKS / 30.0f;
+            return ((float)MY_CLOCKS / 30.0f);
         else if (mode == FIFTY_FRAMES)
-            scaler = (float)MY_CLOCKS / 50.0f;
+            return ((float)MY_CLOCKS / 50.0f);
         else if (mode == SIXTY_FRAMES)
-            scaler = (float)MY_CLOCKS / 60.0f;
+            return ((float)MY_CLOCKS / 60.0f);
     #endif
         else if (mode == MILLI_SECONDS)
-            scaler = MY_CLOCKS / 1000;
+            return (MY_CLOCKS / 1000);
         else if (mode == CENTI_SECONDS)
-            scaler = MY_CLOCKS / 100;
+            return (MY_CLOCKS / 100);
         else if (mode == DECI_SECONDS)
-            scaler = MY_CLOCKS / 10;
+            return (MY_CLOCKS / 10);
         else if (mode == SECONDS)
-            scaler = MY_CLOCKS;
+            return MY_CLOCKS;
         else if (mode == MINUTES)
-            scaler = MY_CLOCKS * 60;
+            return (MY_CLOCKS * 60);
         else if (mode == HOURS)
-            scaler = MY_CLOCKS * 60 * 60;
+            return (MY_CLOCKS * 60 * 60);
         else
-            scaler = MY_CLOCKS;
+            return MY_CLOCKS;
 }
 
--- trunk/Timer.h
+++ trunk/Timer.h
@@ -40,12 +40,13 @@ class Timer
             pausedTicks = 0;
             is_Paused = false;
             is_Started = false;
-            mode = MILLI_SECONDS;   // default value
+            timeMode = MILLI_SECONDS;   // default value
             #ifdef PENJIN_FIXED
                 scaler = 1;
             #else
                 scaler = 1.0f;
             #endif
+            offset = 0;
         }
         Timer(const TimerScalers& scale)
         {
@@ -53,21 +54,30 @@ class Timer
             pausedTicks = 0;
             is_Paused = false;
             is_Started = false;
-            mode = scale;
-            calcScaler();
+            timeMode = scale;
+            scaler = calcScaler(scale);
+            offset = 0;
         }
 
         void setMode(const TimerScalers& mode)       // Set the timer scaler
         {
-            this->mode = mode;
-            calcScaler();
+            timeMode = mode;
+            scaler = calcScaler(mode);
         }
-        TimerScalers getMode()const{return mode;}
+        TimerScalers getMode()const{return timeMode;}
+
+        void setOffset(CRint o) {offset = o * scaler;};
+        void setOffset(CRint o, const TimerScalers& mode) {offset = o * calcScaler(mode);};
+        int getOffset() {return offset / scaler;};
+        int getOffset(const TimerScalers& mode) {return offset / calcScaler(mode);};
 
         int getScaledTicks(const TimerScalers& mode)
         {
-            setMode(mode);
-            return getScaledTicks();
+            #ifdef PENJIN_FIXED
+                return Fixed(getTicks() / calcScaler(mode)).intValue>>16;
+            #else
+                return (int)((float)getTicks() / calcScaler(mode));
+            #endif
         }
         int getScaledTicks() const              // Return the scaled elapsed time
         {
@@ -92,9 +102,9 @@ class Timer
                 {
                     //Return the current time minus the start time
                     #ifndef PENJIN_SYS_TIMER
-                        return SDL_GetTicks() - startTicks;
+                        return SDL_GetTicks() - startTicks + offset;
                     #else
-                        return clock() - startTicks;
+                        return clock() - startTicks + offset;
                     #endif
                 }
             }
@@ -158,18 +168,23 @@ class Timer
 
         /// Set a custom timer resolution.
         #ifdef PENJIN_FIXED
-            void setScaler(CRFixed scale){setMode(CUSTOM);scaler =scale;}
+            void setScaler(CRFixed scale){setMode(CUSTOM);scaler = scale;}
             Fixed getScaler(){return scaler;}
         #else
-            void setScaler(CRfloat scale){setMode(CUSTOM);scaler =scale;}
+            void setScaler(CRfloat scale){setMode(CUSTOM);scaler = scale;}
             float getScaler()const{return scaler;}
         #endif
     protected:
-        void calcScaler();   // Calculate the resolution of timer updates
+        #ifdef PENJIN_FIXED
+        Fixed calcScaler(const TimerScalers& mode);   // Calculate the resolution of timer updates
+        #else
+        float calcScaler(const TimerScalers& mode);
+        #endif
 
-        TimerScalers mode;
+        TimerScalers timeMode;
         int startTicks;
         int pausedTicks;
+        int offset;
         bool is_Paused;
         bool is_Started;
         #ifdef PENJIN_FIXED
