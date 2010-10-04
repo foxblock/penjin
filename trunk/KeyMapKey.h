71ec919cab9f8e1c1a87f7e05aeb84b2a924d793
Wizard.Stan <Wizard.Stan@10f69238-722c-11de-8fed-f74124958806>
2010-10-04 01:45:52 +0000

    Add joystick (and Pandora nub) support

--- trunk/KeyMapKey.h
+++ trunk/KeyMapKey.h
@@ -10,14 +10,16 @@ class KeyMapKey : public KeyMap
         KeyMapKey();
         KeyMapKey(CRstring SJ,CRstring k,CRuchar devID){setKey(k);setSJMap(SJ);setDeviceID(devID);device = DEV_KEYBOARD;}
         KeyMapKey(CRstring k);
-        #if defined(PLATFORM_PANDORA) && (defined(PENJIN_ES) || defined(PENJIN_ES2))
+        #if defined(PLATFORM_PANDORA) && !defined(PENJIN_SDL_INPUT)
+        // && (defined(PENJIN_ES) || defined(PENJIN_ES2))
             KeyMapKey(__u16 k);
         #else
             KeyMapKey(SDLKey k);
         #endif
         virtual ~KeyMapKey();
 
-        #if defined(PLATFORM_PANDORA) && (defined(PENJIN_ES) || defined(PENJIN_ES2))
+        #if defined(PLATFORM_PANDORA) && !defined(PENJIN_SDL_INPUT)
+        // && (defined(PENJIN_ES) || defined(PENJIN_ES2))
             void setBind(const __u16& k,const SIMPLEJOY_MAP& t,CRuchar devID){key=k;target=t;deviceID = devID;}
             __u16 getKey()const{return key;}
         #else
@@ -27,7 +29,8 @@ class KeyMapKey : public KeyMap
 
     private:
         void setKey(CRstring key);
-        #if defined(PLATFORM_PANDORA) && (defined(PENJIN_ES) || defined(PENJIN_ES2))
+        #if defined(PLATFORM_PANDORA) && !defined(PENJIN_SDL_INPUT)
+        // && (defined(PENJIN_ES) || defined(PENJIN_ES2))
             __u16 key;
         #else
             SDLKey key;
