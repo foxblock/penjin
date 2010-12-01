d375dd86a8472c622fbf4a0e6a5cea604b02796d
pokeparadox <pokeparadox@10f69238-722c-11de-8fed-f74124958806>
2010-12-01 14:16:06 +0000

    Update: Penjin now has LGPL licensing info in each header. And Licensing files included.

--- trunk/Achievement.h
+++ trunk/Achievement.h
@@ -1,3 +1,21 @@
+/*
+	Penjin is Copyright (c)2005, 2006, 2007, 2008, 2009, 2010 Kevin Winfield-Pantoja
+
+	This file is part of Penjin.
+
+	Penjin is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Penjin is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with Penjin.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef ACHIEVEMENT_H
 #define ACHIEVEMENT_H
 
@@ -69,7 +87,15 @@ class Achievement
         virtual string getDescription() const {return descr;};
         virtual void setDescription(CRstring newDescr) {if (newDescr.size() > 0) descr = newDescr;};
         virtual const AnimatedSprite* getIcon() const {return &icon;};
-        virtual void setIcon(string fileName) {icon.loadFrames(fileName,2,1); icon.disableTransparentColour();}; // icon should be square
+        virtual void setIcon(string fileName)
+        {
+            icon.loadFrames(fileName,2,1);
+            #ifdef PENJIN_SDL
+                icon.disableTransparentColour();
+            #else
+                cout << "FIXME - Image.disableTransparentColour()" << endl;
+            #endif
+        }; // icon should be square
 
         virtual bool getSecret() const {return secret;};
         virtual void setSecret(bool newState) {secret = newState;};
